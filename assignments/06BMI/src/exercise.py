e24cdbbea468ebb92a3476034bddda93cb85507a
A01541326 <110918360+A01541326@users.noreply.github.com>
2022-08-21 20:03:08 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -2,6 +2,11 @@ def main():
     #escribe tu código abajo de esta línea
     peso = float(input("Peso en kg:"))
     altura = float(input("Altura en m:"))
+
+    IMC = peso / (altura*2)
+    if IMC < 20:
+        print(PESO BAJO)
+    elif 
     
 
 if __name__=='__main__':
